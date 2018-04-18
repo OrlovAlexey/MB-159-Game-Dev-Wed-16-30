d6ee54b82aeb0b0256ef3021e2cee5b4bcca1db6
Nesqvic <vicnik83@gmail.com>
2018-04-18 17:05:14 +0300

    18.04.2018

---
+++ docs/lesson08/lesson08.js
@@ -0,0 +1,12 @@
+// var pass = prompt("Введите пароль", "****");
+
+// while (pass != "pass")
+// {
+// 	alert("Пароль неверный.\nДоступ запрещён!");
+// 	pass = prompt("Введите пароль", "****");
+// }
+
+// var header = document.createElement("h3");
+// header.innerHTML = "Доступ разрешён!!!";
+// document.body.appendChild(header);
+
